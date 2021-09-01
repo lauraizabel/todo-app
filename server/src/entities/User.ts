e1c333dd7d6830c2d274550a89e30cb0dcb360f6
--global <lauraizabel8569@gmail.com>
2021-09-01 13:15:38 -0300

    User entity and model

---
+++ server/src/entities/User.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+  @Column()
+  firstName: string;
+  @Column()
+  lastName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
---
+++ server/src/routes/publicRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+//register new user
+router.post("/register");
+
+//login
+router.post("login");
+
+export default router;
