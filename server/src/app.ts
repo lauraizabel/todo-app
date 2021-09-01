9cb2a63d3851402f5fc51c1efb6be0152c07538d
--global <lauraizabel8569@gmail.com>
2021-09-01 13:04:06 -0300

    fix app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,38 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { createConnection } from "typeorm";
+import config from "./database/config";
 
 const app = express();
 
-app.listen(3080, () => {
-  console.log("app listen in 3080 port.");
+app.use(cors());
+app.use(express.json({}));
+app.options("/", cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
 });
+
+const PORT = process.env.PORT || 3080;
+
+createConnection(config)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server is running on port", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Unable to connect db", err);
+    process.exit(1);
+  });
