e32318800f9119155348634e139e49bd46b5e361
--global <lauraizabel8569@gmail.com>
2021-09-02 13:33:51 -0300

    routes for tasks

--- server/src/routes/privateRoutes.ts
+++ server/src/routes/privateRoutes.ts
@@ -3,6 +3,8 @@ import isAuthenticated from "../middleware/isAuthenticated";
 import createCategoryController from "../useCases/category/CreateCategory";
 import deleteCategoryController from "../useCases/category/DeleteCategory";
 import updateCategoryController from "../useCases/category/UpdateCategory";
+import createTaskController from "../useCases/task/CreateTask";
+import deleteTaskController from "../useCases/task/DeleteTask";
 import updateUserController from "../useCases/user/UpdateUser";
 
 const router = Router();
@@ -14,5 +16,11 @@ router.put("/user", updateUserController.handle);
 router.post("/category", createCategoryController.handle);
 router.put("/category/:id", updateCategoryController.handle);
 router.delete("/category/:id", deleteCategoryController.handle);
+
+router.post("/task", createTaskController.handle);
+router.delete("/task/:id", deleteTaskController.handle);
+router.get("/tasks");
+router.get("/task/:id");
+router.put("/task/:id");
 
 export default router;
